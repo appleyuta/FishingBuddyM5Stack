e855b678bc3b80905db3d936f07fd54cf9aa8cbf
Kobayashi Yuta <appleappleyuta@gmail.com>
2024-08-24 20:53:46 +0900

    閾値調整機能を追加、Bluetooth接続プログラムを修正

--- FishingBuddyM5Stack.ino
+++ FishingBuddyM5Stack.ino
@@ -14,6 +14,14 @@ extern "C"
 }
 #endif
 
+
+// See the following for generating UUIDs:
+// https://www.uuidgenerator.net/
+#define SERVICE_UUID "a01d9034-21c3-4618-b9ee-d6d785b218c9"
+#define CHARACTERISTIC_UUID "f98bb903-5c5a-4f46-a0f2-dbbcf658b445"
+
+#define DEFAULT_HIT_THRESHOLD 50
+
 // IMU6886のインスタンスを作成
 IMU_6886 imu6886;
 
@@ -25,37 +33,88 @@ float accZ = 0;
 float gyroX = 0;
 float gyroY = 0;
 float gyroZ = 0;
+
+// 当たり判定閾値
+uint16_t hit_threshold = DEFAULT_HIT_THRESHOLD;
+
 // Bluetooth送信クラス
 BLECharacteristic *pCharacteristic = NULL;
+BLEServer *pServer = NULL;
 
-// See the following for generating UUIDs:
-// https://www.uuidgenerator.net/
-#define SERVICE_UUID "a01d9034-21c3-4618-b9ee-d6d785b218c9"
-#define CHARACTERISTIC_UUID "f98bb903-5c5a-4f46-a0f2-dbbcf658b445"
+// Bluetooth接続状態
+static bool deviceConnected = false;
+static bool oldDeviceConnected = false;
 
 
-void startService(BLEServer *pServer)
-{
+// Bluetooth LE Change Connect State
+class MyServerCallbacks: public BLEServerCallbacks {
+    void onConnect(BLEServer* pServer) {
+      deviceConnected = true;
+      M5.Lcd.setCursor(10, 210);
+      M5.Lcd.println("Bluetooth: connect    ");
+    };
+
+    void onDisconnect(BLEServer* pServer) {
+      deviceConnected = false;
+      M5.Lcd.setCursor(10, 210);
+      M5.Lcd.println("Bluetooth: disconnect");
+      
+    }
+};
+
+
+// Bluetooth LE initialize
+void initBLE() {
+  // Create the BLE Device
+  BLEDevice::init("Fishing Buddy BLE Server");
+
+  // Create the BLE Server
+  pServer = BLEDevice::createServer();
+  pServer->setCallbacks(new MyServerCallbacks());
+
+  // Create the BLE Service
   BLEService *pService = pServer->createService(SERVICE_UUID);
 
+  // Create a BLE Characteristic
   pCharacteristic = pService->createCharacteristic(
-      CHARACTERISTIC_UUID,
-      BLECharacteristic::PROPERTY_NOTIFY
-      // BLECharacteristic::PROPERTY_READ |
-      // BLECharacteristic::PROPERTY_WRITE
-      );
-  pCharacteristic->addDescriptor(new BLE2902()); // Descriptorを定義しておかないとClient側でエラーログが出力される
+                        CHARACTERISTIC_UUID,
+                        BLECharacteristic::PROPERTY_NOTIFY
+                        );
+  
+  pCharacteristic->addDescriptor(new BLE2902());  // Descriptorを定義しておかないとClient側でエラーログが出力される
 
+  // BLECharacteristic * pRxCharacteristic = pService->createCharacteristic(
+  //                      CHARACTERISTIC_UUID_RX,
+  //                     BLECharacteristic::PROPERTY_WRITE
+  //                   );
+
+  // pRxCharacteristic->setCallbacks(new MyCallbacks());
+
+  // Start the service
   pService->start();
+
+  // Start advertising
+    BLEAdvertising *pAdvertising = BLEDevice::getAdvertising();
+    pAdvertising->addServiceUUID(SERVICE_UUID);
+    pAdvertising->setScanResponse(true); // trueにしないと、Advertising DataにService UUIDが含まれない。
+    // minIntervalはデフォルトの20でとくに問題なさそうなため、setMinPreferredは省略
+    BLEDevice::startAdvertising();
 }
 
-void startAdvertising()
-{
-  BLEAdvertising *pAdvertising = BLEDevice::getAdvertising();
-  pAdvertising->addServiceUUID(SERVICE_UUID);
-  pAdvertising->setScanResponse(true); // trueにしないと、Advertising DataにService UUIDが含まれない。
-  // minIntervalはデフォルトの20でとくに問題なさそうなため、setMinPreferredは省略
-  BLEDevice::startAdvertising();
+
+void loopBLE() {
+    // disconnecting
+    if (!deviceConnected && oldDeviceConnected) {
+        delay(1000); // give the bluetooth stack the chance to get things ready
+        pServer->startAdvertising(); // restart advertising
+        Serial.println("startAdvertising");
+        oldDeviceConnected = deviceConnected;
+    }
+    // connecting
+    if (deviceConnected && !oldDeviceConnected) {
+    // do stuff here on connecting
+        oldDeviceConnected = deviceConnected;
+    }
 }
 
 
@@ -87,7 +146,23 @@ float calcVectorNorm(float X, float Y, float Z) {
 // 当たり判定
 bool judgeHit(float X, float Y, float Z) {
   float vec_norm = calcVectorNorm(X, Y, Z);
-  return vec_norm >= 600;
+  return vec_norm >= hit_threshold;
+}
+
+// 当たり判定の閾値を変更する
+void changeHitThreshold() {
+  // 左側のボタンを押すと、閾値を下げる
+  if (M5.BtnA.isPressed() == 1) {
+    hit_threshold -= 5;
+  }
+  // 真ん中のボタンを押すと、閾値をデフォルトに戻す
+  if (M5.BtnB.isPressed() == 1) {
+    hit_threshold = DEFAULT_HIT_THRESHOLD;
+  }
+  // 右側のボタンを押すと、閾値を上げる
+  if (M5.BtnC.isPressed() == 1) {
+    hit_threshold += 5;
+  }
 }
 
 void setup()
@@ -98,10 +173,7 @@ void setup()
   Serial.begin(115200);
 
   // Bluetooth初期化、通信開始
-  BLEDevice::init("Fishing Buddy BLE Server");
-  BLEServer *pServer = BLEDevice::createServer();
-  startService(pServer);
-  startAdvertising();
+  initBLE();
 
   M5.Lcd.fillScreen(BLACK);
   M5.Lcd.setTextSize(2);
@@ -110,14 +182,19 @@ void setup()
   M5.Lcd.setCursor(10, 25);
   M5.Lcd.println("  X      Y       Z");
   M5.Lcd.setCursor(10, 210);
-  M5.Lcd.println("Advertising!");
+  M5.Lcd.println("Bluetooth: disconnect");
 
   imu6886.Init(21, 22);
 }
 
 void loop()
 {
-    // put your main code here, to run repeatedly:
+  // put your main code here, to run repeatedly:
+  loopBLE();
+
+  // ボタンが押されたときに当たり判定閾値を変更
+  changeHitThreshold();
+
   imu6886.getGyroData(&gyroX, &gyroY, &gyroZ);
   imu6886.getAccelData(&accX, &accY, &accZ);
 
@@ -138,7 +215,7 @@ void loop()
   M5.Lcd.setCursor(10, 150);
   M5.Lcd.printf("gyro_vec_norm: %.2f", vec_norm);
   M5.Lcd.setCursor(10, 170);
-  M5.Lcd.printf("judge_hit: %d", judge_hit);
+  M5.Lcd.printf("thresh: %d, hit: %s", hit_threshold, (judge_hit ? "true " : "false"));
 
   // BLE送信
   // BLEでのデータ通知用バッファを定義
@@ -152,5 +229,6 @@ void loop()
   pCharacteristic->setValue(dataBuffer, 13);
   pCharacteristic->notify(); // 通知を送信
 
+  M5.update();
   delay(100);
 }
